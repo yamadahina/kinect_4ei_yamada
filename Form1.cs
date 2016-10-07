3500dded1a4a0193d1c09bbf32653199e71ac495
yamadahina <i49538hy@stu.fukui-nct.ac.jp>
2016-10-07 14:30:12 +0900

    2016/10/07 こっちが本当

---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+// ビットマップを生成するため
+using System.Drawing.Imaging;
+
+// Kinectのセンサクラス
+// Microsoft.Kinectを参照に追加
+using Microsoft.Kinect;
+
+// メモリ管理の最適化のため
+// System.Runtime.Serializationを参照に追加
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        /// <summary>
+        /// kinectのセンサクラス
+        /// </summary>
+        KinectSensor kinect;
+
+        /// <summary>
+        /// kinectから取得したRGBデータ
+        /// （byte型配列）
+        /// </summary>
+        byte[] imageData;
+
+        /// <summary>
+        /// 実画像
+        /// （ビットマップ形式）
+        /// </summary>
+        Bitmap ImageBitmap;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // ピットマップの初期化
+            ImageBitmap = new Bitmap(640,480);
+
+            // kinectの初期化
+            kinect = KinectSensor.KinectSensors[0];
+
+            // カラー画像の取得を開始する
+            kinect.ColorFrameReady += new EventHandler<ColorImageFrameReadyEventArgs>(ColorImageReady);
+            kinect.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+            // Kinectを起動する
+            kinect.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 画像の保存
+            ImageBitmap.Save(DateTime.Now.Year + " - " + DateTime.Now.Month + "-" + DateTime.Now.Day + "-" + DateTime.Now.Hour + "-" + DateTime.Now.Minute + "-" + DateTime.Now.Second + ".bmp");
+        }
+
+        /// <summary>
+        /// カラー画像の取得
+        /// </summary>
+
+
+        void ColorImageReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            // kinectからカラーイメージを取得
+            ColorImageFrame image = e.OpenColorImageFrame();
+
+            // imageがnullだった場合処理しない
+            if (image != null)
+            {
+                // imageData配列の初期化
+                imageData = new byte[image.PixelDataLength];
+
+                // imageのピクセルデータをpixelDataへコピーする
+                image.CopyPixelDataTo(imageData);
+
+                // imageDataからビットマップへ変換する
+                ImageBitmap = toBitmap(imageData, ImageBitmap.Width, ImageBitmap.Height);
+
+                // ピクチャーボックスへ反映
+                pictureBox1.Image = ImageBitmap;
+
+            }
+        }
+
+        /// 取得データをビットマップデータに変換
+        /// </summary>
+        /// <param name="pixels">kinectで取得したbyte[]配列</param>
+        /// <param name="width">横サイズ</param>
+        /// <param name="height">縦サイズ</param>
+        /// <returns></returns>
+        public static Bitmap toBitmap(byte[] pixels, int width, int height)
+        {
+            // pixelsに何も入っていない場合nullを返す
+            if (pixels == null)
+                return null;
+
+            // ビットマップの初期化
+            var bitmap = new Bitmap(width, height, PixelFormat.Format32bppRgb);
+
+            // システムメモリへロック
+            var data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+
+            // メモリデータのコピー
+            Marshal.Copy(pixels, 0, data.Scan0, pixels.Length);
+
+            // システムメモリのロック解除
+            bitmap.UnlockBits(data);
+
+            return bitmap;
+        }
+
+      
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "撮影";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 526);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "撮影画面";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
